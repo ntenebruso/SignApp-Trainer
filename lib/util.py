70e32b58a39e8befcb71ff059f1f88fd00da21cc
Nick Tenebruso <nickten2006@gmail.com>
2023-11-28 13:42:34 -0500

    switch to poetry for package management

--- lib/util.py
+++ lib/util.py
@@ -2,12 +2,12 @@ import configparser
 import os
 import sys
 
-config = configparser.ConfigParser()
-
 if getattr(sys, "frozen", False):
     bundle_dir = sys._MEIPASS
 else:
     bundle_dir = os.getcwd()
+
+config = configparser.ConfigParser()
 
 config.read(os.path.join(bundle_dir, "config.ini"))
 train_config = config["train"]
